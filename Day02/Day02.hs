7c5e3dc4a358d93d7f09c525cd080c0355c4b74a
Connor Clark <connor@psyleft.com>
2025-12-02 16:01:20 -0500

    Day 02

---
+++ Day02/Day02.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE TypeApplications #-}
+
+module Day02 where
+
+import Data.List
+
+type Id = String
+
+splitOn :: Eq a => a -> [a] -> [[a]]
+splitOn k = foldr f [[]]
+  where f x acc@(a:as)
+          | x == k    = []:acc
+          | otherwise = (x:a):as
+
+parseRange :: String -> [Id]
+parseRange s =
+  let [btm, top] = map (read @Int) $ splitOn '-' s
+  in map show $ [btm..top]
+
+halve :: [a] -> ([a], [a])
+halve xs = splitAt (length xs `div` 2) xs
+
+-- Predicate for part1 rules
+isInvalid :: Id -> Bool
+isInvalid id =
+  case halve id of
+    (l, r) -> if (l == r) then True else False
+
+-- Predicate for part2 rules
+isInvalid' :: Id -> Bool
+isInvalid' id =
+  case halve id of
+    (l, _) -> any f $ tail $ inits l
+  where f x = id `isPrefixOf` cycle x && (length id) `mod` (length x) == 0
+
+part1 :: [Id] -> Int
+part1 = sum . map (read @Int) . filter isInvalid
+
+part2 :: [Id] -> Int
+part2 = sum . map (read @Int) . filter isInvalid'
+
+main :: IO ()
+main = do
+  f <- readFile "input.txt"
+  let input = concat $ map parseRange $ splitOn ',' f
+  print $ part1 input
